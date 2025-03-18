5834eaf59c53429a1990d0ca928068282f03081c
Soerig <erickmurilloloor@gmail.com>
2025-03-18 12:48:20 -0500

    Actualización del app.js

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaAmigos = [0];
-let posiciónAleatoria = 0;
+let listaAmigos = [];
+
+function agregarAmigo() {
+
+    let amigos = document.querySelector('#amigo').value;
+    let elementoHTML = document.getElementById('listaAmigos');
+    if(amigos == ''){
+        alert("Por favor, inserte un nombre");
+    }else{
+        listaAmigos.push(amigos);
+        elementoHTML.innerHTML += `<li>${amigos}</li>`;
+    };
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo() {
+    
+    let resultadoHTML = document.getElementById('resultado');
+    if(listaAmigos.length === 0){
+        alert("La lista está vacia, ingrese nombres...");
+    }else{
+        let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        resultadoHTML.innerHTML = `<li>Tu amigo secreto es: ${listaAmigos[indiceAleatorio]}</li>`;
+    }
+}
+
+
